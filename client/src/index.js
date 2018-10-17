bdf5cc96e44db7e8023d1e69b873b3ac8f0b642d
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-16 19:24:48 -0700

    install pm2, move BP, rename index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from '../redux/store/index.js';
-import Root from './Root'
-import * as serviceWorker from './serviceWorker';
+import Root from './Root.jsx';
+import * as serviceWorker from './serviceWorker.js';
 import './styles/css/index.css';
 
 ReactDOM.render(
