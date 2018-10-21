822590dc1afcfe8e9c3b2c8ada8963f6ae543040
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-20 23:33:56 -0700

    modularize/refactor reducers

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,9 +1,23 @@
-export const newConnection = () => ({ 
-  type: 'server/new_connection', 
-  payload: { content: 'New connection established from React' } 
-});
+function actionCreator(type, ...argNames) {
+  return function(...args) {
+    const action = { type };
+    argNames.forEach((arg, i) => {
+      action[argNames[i]] = args[i];
+    });
+    return action;
+  };
+}
 
-export const newMessage = (msg) => ({ 
-  type: 'server/message', 
-  payload: { msg }
-});
+const message = 'server/message';
+const newConn = 'server/new_connection';
+const fileChange = 'server/file_change';
+const changeTheme = 'server/change_theme';
+
+export const newConnection = actionCreator(newConn, 'payload');
+
+export const newMessage = actionCreator(message, 'payload');
+
+export const updateFile = actionCreator(fileChange, 'payload');
+
+export const changeMirrorTheme = actionCreator(changeTheme, 'payload');
+
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import createSocketMW from 'redux-socket.io';
 import socketIO from 'socket.io-client';
 
-import rootReducer from '../reducers/rootReducer';
+import { reducers } from '../reducers/rootReducer';
 
 const io = socketIO.connect('localhost:8080');
 const socketMW = createSocketMW(io, 'server/');
 
 
 
-const store = applyMiddleware(socketMW)(createStore)(rootReducer);
+const store = applyMiddleware(socketMW)(createStore)(reducers);
 
 export default store;
