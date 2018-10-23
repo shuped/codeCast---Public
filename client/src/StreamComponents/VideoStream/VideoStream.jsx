e121ba84e8b5577f707efd499c0515086480f087
jeffreypang911 <jeffreypang911@gmail.com>
2018-10-23 22:39:37 +0000

    adds styled stream page

--- client/src/StreamComponents/VideoStream/VideoStream.jsx
+++ client/src/StreamComponents/VideoStream/VideoStream.jsx
@@ -4,9 +4,7 @@ class Video extends Component {
   render() {
     return (
       <div className='video-main'>
-        <iframe width="420" height="315"
-          src="https://www.youtube.com/watch?v=hHW1oY26kxQ">
-        </iframe>
+        <iframe className='iframe' src="https://www.youtube.com/embed/hHW1oY26kxQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     );
   };
