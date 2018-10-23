af77e37116eba0181641642fbe98aa28a0d93b72
erik-mackie <erik.mackie@gmail.com>
2018-10-23 23:00:05 +0000

    started mode script

--- client/src/StreamComponents/fileDisplay/LiveCodeDisplay.jsx
+++ client/src/StreamComponents/fileDisplay/LiveCodeDisplay.jsx
@@ -9,6 +9,7 @@ import StyleList from './StyleDisplay.jsx';
 
 // syntax highlighting
 import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/mode/python/python.js';
 
 // add ons 
 import 'codemirror/addon/selection/active-line.js';
@@ -19,7 +20,8 @@ import 'codemirror/addon/edit/closetag.js';
 import { updateFile, changeMirrorTheme } from '../../redux/actions/index';
 
 const mapStateToProps = (state) => ({
-  theme: state.theme
+  theme: state.theme,
+  // activeFileContents: 'const stuff = [1,2,3,4]; \n    var test = function() {a + b}; '
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -32,8 +34,16 @@ class LiveCodeDisplay extends Component {
   constructor(props) {  
     super(props); 
     this.state = {
-      theme: 'dracula'
+      theme: 'dracula',
+      activeFileContents: `round1 = int(raw_input("Enter score for round 1: ")); \n    round2 = int(raw_input("Enter score for round 2: ")) `
     };
+  }
+
+  changeDisplayMode = () => {
+    editor.setOption("mode", modeInput.value);
+    CodeMirror.autoLoadMode(editor, modeInput.value);
+
+
   }
 
   changeDisplayTheme = evt => {
@@ -47,7 +57,7 @@ class LiveCodeDisplay extends Component {
       lineNumbers: true,
       styleActiveLine: true,
       matchBrackets: true,
-      mode: 'javascript', // reference options in folder
+      mode: 'python', // reference options in folder
       theme: this.state.theme
       //  readOnly: 'nocursor'
     };
@@ -55,7 +65,7 @@ class LiveCodeDisplay extends Component {
     return (
       <div className="file-container">
         <CodeMirror 
-          value={'const stuff = [1,2,3,4]; \n    var test = function() {a + b}; '}
+          value={ this.state.activeFileContents }
           options={ options } 
           // value={this.state.code}
         />
@@ -70,3 +80,18 @@ class LiveCodeDisplay extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LiveCodeDisplay);
+
+
+
+// CodeMirror.modeURL = "../mode/%N/%N.js";
+// var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+//   lineNumbers: true
+// });
+// var modeInput = document.getElementById("mode");
+// CodeMirror.on(modeInput, "keypress", function(e) {
+//   if (e.keyCode == 13) change();
+// });
+// function change() {
+//    editor.setOption("mode", modeInput.value);
+//    CodeMirror.autoLoadMode(editor, modeInput.value);
+// }
