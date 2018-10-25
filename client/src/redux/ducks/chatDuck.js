220b7cadd5d21723ddcddd314d82cd4ab6ddde54
Spencer Shupe <shupe@protonmail.com>
2018-10-25 05:57:53 -0700

    duck refactor

---
+++ client/src/redux/ducks/chatDuck.js
@@ -0,0 +1,39 @@
+// Outgoing
+const OUTGOING_MESSAGE = 'server/message';
+const NEW_CONNECTION = 'server/new_connection';
+
+// Incoming
+const INCOMING_MESSAGE = 'NEW_MESSAGE'
+
+// Action Creator
+const newConnection = () => ({ type: NEW_CONNECTION, payload: {} });
+const newMessage = (message) => ({ type: OUTGOING_MESSAGE, payload: { message } });
+
+// CHAT REDUCER
+const chatState = { messages: [randomMessage()] }
+const chatReducer = (state = chatState, action) => {
+  switch(action.type) {
+    case INCOMING_MESSAGE:
+      console.log('message action', action.payload);
+      return {...state, messages: action.payload};
+
+    default:
+      return state;
+  }
+};
+
+export default chatReducer
+
+// State init. utility
+function randomMessage() {
+  const messageBank = [
+    {
+      type: 'messages',
+      id: uuid(),
+      timestamp: new Date(),
+      user: { username: 'Joel@LHL', userColor: '#101010'},
+      content: 'A monad is just a monoid in the category of endofunctors, what\'s the problem?'
+    }
+  ]
+  return messageBank[Math.floor(Math.random() * messageBank.length)]
+}
--- client/src/redux/ducks/fileDuck.js
+++ client/src/redux/ducks/fileDuck.js
@@ -1,16 +1,16 @@
-// Types
+// Outgoing
+
+// Incoming
 const FILE_UPDATE = 'FILE_UPDATE'
 
-// Actions:
-//  Outgoing
-
-//  Incoming
+// Action Creators
 
 // FILE REDUCER
 const fileState = { activeFileContents: 'Welcome to codeCast! Click on a file in the explorer to see its contents!'}
 const fileReducer = (state = fileState, action) => {
   switch (action.type) {
     case FILE_UPDATE:
+      console.log('File Update', action.payload)
       return { ...state, activeFileContents: action.payload }
     
     default:
