a482f70b48d3f3456074630fa114b4853789fb0a
Spencer Shupe <shupe@protonmail.com>
2018-10-25 04:44:11 -0700

    mounted terminal with refs properly. wrapper for terminal.

--- client/src/StreamComponents/Terminal/Terminal.jsx
+++ client/src/StreamComponents/Terminal/Terminal.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Terminal } from 'xterm';
+import * as fit from 'xterm/lib/addons/fit/fit';
 import socket from 'socket.io-client';
 import 'xterm/src/xterm.css';
 
 class Console extends Component {
-  
+  constructor() {
+    super();
+    this.__term = null;
+    this.terminal = null;
+    this.setTermRef = (e) => {
+      this.__term = e;
+      this.terminal.open(this.__term);
+      this.terminal.fit()
+    }
+  }
 
   componentWillMount() {
+    Terminal.applyAddon(fit);
     this.terminal = new Terminal({scrollback: 99999999});
   }
 
@@ -18,11 +29,11 @@ class Console extends Component {
   }
 
   render() {
-    
-    this.terminal.open(this.__term)
     return (
-      <div className='terminal-main'
-        ref={(e) => this.__term = e}>
+      <div className="wrapper">
+        <div className='terminal-main'
+          ref={this.setTermRef}>
+        </div>
       </div>
     );
   };
