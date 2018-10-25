b57b40c04269bc62cef053c395ea2e675484abf9
erik-mackie <erik.mackie@gmail.com>
2018-10-25 01:43:57 +0000

    stream population scripting andg and first round css

--- client/src/ViewComponents/IndexViews/ActiveStreams/ActiveStreams.jsx
+++ client/src/ViewComponents/IndexViews/ActiveStreams/ActiveStreams.jsx
@@ -9,7 +9,7 @@ class ActiveStreams extends Component {
   constructor(props) {  
     super(props); 
     this.state = {
-      scheduledStreams: [
+      activeStreams: [
         {
           title: 'Python',
           presentor: 'Spencer',
@@ -30,6 +30,27 @@ class ActiveStreams extends Component {
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
           ActiveStreamId: 3,
           imagePath: null
+        },
+        {
+          title: 'Node',
+          presentor: 'Space G',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+          ActiveStreamId: 1,
+          imagePath: null
+        },
+        {
+          title: 'Ruby',
+          presentor: 'Mandy',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+          ActiveStreamId: 2,
+          imagePath: null
+        },
+        {
+          title: 'Express',
+          presentor: 'Silvia',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet,',
+          ActiveStreamId: 3,
+          imagePath: null
         }
       ]
     };
@@ -43,18 +64,18 @@ class ActiveStreams extends Component {
     const { title, presentor, description, ActiveStreamId, imagePath } = props;
     // missing image path
     return (
-      <div className="activeStreamCard" key={(Math.random()*10).toString().slice(2,6)}>
+      <div className="activeStreamCard" key={ (Math.random()*10).toString().slice(2,6) }>
         <div className="banner">
           <div>
             <h1>{ title }</h1>
             <h2>{ presentor }</h2>
           </div>
-          <img src="#imagePath" />
+          {/* <img src="#imagePath" /> */}
+          <div className="imagePlaceholder">p</div>
         </div>
         <div className="description-container">
-          <p>{ description }</p>
+          { description }
         </div>
-          
       </div>
     )
   }
