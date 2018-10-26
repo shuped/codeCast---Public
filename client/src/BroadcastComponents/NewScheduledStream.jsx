c34f1b0016ab943665d8dd66822f7dae8548d79f
erik-mackie <erik.mackie@gmail.com>
2018-10-26 22:27:30 +0000

    input components place in html

--- client/src/BroadcastComponents/NewScheduledStream.jsx
+++ client/src/BroadcastComponents/NewScheduledStream.jsx
@@ -1,33 +1,99 @@
 import React from 'react';
 import { Button } from 'antd';
+import { Input, Select, DatePicker } from 'antd';
+
+const InputGroup = Input.Group;
+const Option = Select.Option;
 
 class ScheduleNewStream extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      newStream: {
-        
-      }
+      title: 'placeholder',
+      user: 'Spencer Mc-Whhite'
+      description: 'asdasdas',
+      scheduledDate: '',
+      languageImage: ''
     }
+
   }
 
-
+  //handlers
+  TitleOnChange = (event) => {
+    this.setState({title: event.target.value});
+  }
   
-  
+  DescriptionOnChange = (event) => {
+    this.setState({description: event.target.value})
+  }
 
+  onChange = (value) => {
+    this.setState({scheduledDate: value})
+  }
+  
+  onOk = (value) => {
+    this.setState({scheduledDate: value})
+    console.log('onOk: ', value);
+  }
+
+  HandleImageSelection = (value) => {
+    this.setState({languageImage: value});
+    console.log(value)
+  }
+
+  HandleSubmit = (event) => {
+    event.preventDefault();
+    //redirect to broadcast page
+
+    console.log(this.state.newStream
+    
+  }
 
   render() {  
- 
-
+    
     return (
 
         <main className="new-stream">
           <header className="header">
-
+            <Button id="dashboard" type="primary" onClick={ console.log("dashboard route")}>Dashboard</Button>
+            <div className="logoPlaceholder">p</div>
           </header>
-          <div className="main">
+          <div className="inputs-container">
+            <h1>New Scheduled Stream</h1>
 
+            <div className="forms-container">
+            <form onSubmit={this.HandleSubmit}>
+              <div className="title-input">
+                <h3>Title:</h3>
+                <input type="text" title={this.state.title} onChange={this.TitleOnChange} />
+              </div>
+              <div className="description-input">
+                <h3>Description:</h3>
+                <textarea type="text" rows="4" cols="90" description={this.state.description} onChange={this.DescriptionOnChange} />
+              </div>
+              <div className=" bottom-container">
+                <DatePicker
+                  className="date-selection"
+                  showTime
+                  format="YYYY-MM-DD HH:mm:ss"
+                  placeholder="Select Time"
+                  onChange={this.onChange}
+                  onOk={this.onOk}
+                />
+                {/* Language image selector */}
+                <InputGroup compact onChange={this.HandleImageSelection} >
+                  <Select className="image-select" defaultValue="default">
+                    <Option value="default">----</Option>
+                    <Option value="image/path1">Node</Option>
+                    <Option value="image/path2">Ruby</Option>
+                  </Select>
+                </InputGroup>
+                <input type="submit" value="Submit" />
+              </div>
+
+            </form>
+            </div>
           </div>
         </main>
       
@@ -45,11 +111,11 @@ class ScheduleNewStream extends React.Component {
 
 // const mapDispatchToProps = (dispatch) => {
 //   return {
-//     deleteStream: (fileID) => dispatch(updateFile(fileID))
+//     newScheduledStream: (fileID) => dispatch(updateFile(fileID))
 //   }
 // }
 
-export default ScheduleNewStream:
+export default ScheduleNewStream;
 // export default connect(null, null)(ActiveStreams);
 
 // data representation
