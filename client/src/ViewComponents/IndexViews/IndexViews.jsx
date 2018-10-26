9fea11d4e903e484e60d2c7c1a67cad9005e160f
jeffreypang911 <jeffreypang911@gmail.com>
2018-10-26 05:04:29 +0000

    adds working three page views

--- client/src/ViewComponents/IndexViews/IndexViews.jsx
+++ client/src/ViewComponents/IndexViews/IndexViews.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ArchivedStreams from './ArchivedStreams/ArchivedStreams.jsx';
+import ScheduledStreams from './ScheduledStreams/ScheduledStreams.jsx';
+import ActiveStreams from './ActiveStreams/ActiveStreams.jsx';
 
 class IndexViews extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			pageValue: '/',
+			pageValue: '/current',
 			erectileState: 'quarter chub'
 		}
 	}
@@ -13,9 +16,34 @@ class IndexViews extends Component {
 		this.setState({pageValue: route})
 		console.log("State:", this.state)
 	}
-
-
+	
 	render() {
+		
+		let pageVal = () => {
+			console.log("HELLOWORLD?")
+			if (this.state.pageValue === '/current') {
+				return (
+					<div className='index-view'>
+						<ActiveStreams />
+					</div>
+				)
+			}
+			if (this.state.pageValue === '/scheduled') {
+				return (
+					<div className='index-view'>
+						<ScheduledStreams />
+					</div>
+				)
+			}
+			if (this.state.pageValue === '/archived') {
+				return (
+					<div className='index-view'>
+						<ArchivedStreams />
+					</div>
+				)
+			}
+		}
+		
 		return (
 			<div>
 				<button className="tablink" onClick={() => this.setRouteState('/current')} id="defaultOpen">
@@ -28,6 +56,7 @@ class IndexViews extends Component {
 					Archived Streams
 				</button>
 					{this.state.pageValue}
+				{pageVal()}
 			</div>
 		)
 	}
