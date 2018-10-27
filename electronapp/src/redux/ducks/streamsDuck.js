87946f4ac45df7115d6e621336f18912fbdd1e59
Spencer Shupe <shupe@protonmail.com>
2018-10-27 10:16:15 -0700

    removed madness, made fetchBroadcasterStreams

--- electronapp/src/redux/ducks/streamsDuck.js
+++ electronapp/src/redux/ducks/streamsDuck.js
@@ -1,37 +1,54 @@
-import { fetchBroadcasterStreams } from './ajaxDuck'
+const axios = require('./api');
 
-// // Outgoing
-// const OUTGOING_MESSAGE = 'server/message';
-// const NEW_CONNECTION = 'server/new_connection';
+// Outgoing
+// async -> fetchBroadcasterStreams, postDeleteStream
 
 // Incoming
-// const INCOMING_MESSAGE = 'NEW_MESSAGE'
-// const INCOMING_NOTIFICATION = 'NEW_NOTIFICATION'
 const BROADCASTER_STREAMS_UPDATE = 'UPDATE_USER_STREAMS'
 
 // Action Creator
 export const updateBroadcasterStreams = (scheduledStreams) => ({ type: BROADCASTER_STREAMS_UPDATE, payload: scheduledStreams  });
 
 // STREAM REDUCER
-
 export const streamsReducer = (state = {}, action) => {
   switch(action.type) {
     case BROADCASTER_STREAMS_UPDATE:
       console.log('message recieved', action.payload);
-      let steamsArray = shitballs()
-      return shitballs(state);
+      return { ...state, scheduledStreams: action.payload }
 
     default:
       return state;
   }
 };
- 
-async function shitballs(state) {
-  let resultArr = [];
-  let resultObj = await fetchBroadcasterStreams(1);
-  for (let item in resultObj) {
-    resultArr.push(resultObj[item]);
-  }
-  return await { ...state, scheduledStreams: resultArr };
+
+// Async Actions
+export const fetchBroadcasterStreams = (userID) => {
+  return function (dispatch) {
+    return axios({
+      method: 'get',
+      url: `/api/scheduledStreams?user_id=${userID}`
+    }).then((streamsJSON) => {
+      console.log(streamsJSON); 
+      // make array so we can map over for jsx elements
+      const scheduledStreams = Object.entries(JSON.parse(streamsJSON))
+        .map(([streamID, stream]) => [streamID, ...stream]);
+      dispatch(updateBroadcasterStreams(scheduledStreams))
+    }).catch((err) => {
+      console.error('Error:', err.data);
+      throw err
+    });
+  };
 }
 
+export const postDeleteStream = (streamID) => {
+  // refactor as above
+  axios({
+    method: 'delete',
+    url: `/api/scheduledStreams?stream_id=${streamID}`
+  }).then((streamsJSON) => {
+    console.log(streamsJSON.data);
+  }).catch((err) => {
+    console.error('Error:', err.data);
+    throw err;
+  });
+}
