18cfddb5e7567729cc6188c587965929a9758548
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-26 21:49:57 -0700

    fix term styles and config server and electron for axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import axios from 'axios';
 //components
 
 import Chat from './StreamComponents/Chat/ChatMain.jsx';
