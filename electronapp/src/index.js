c9236858e445b8100af91d6e51c82f603537e920
Spencer Shupe <shupe@protonmail.com>
2018-10-27 11:38:28 -0700

    insignificant changes as I make pull request

--- electronapp/src/index.js
+++ electronapp/src/index.js
@@ -1,12 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-<<<<<<< HEAD
-var Entry = function(){
-=======
 var Entry = React.createClass({
   render: function() {
->>>>>>> 5c86598303d6c29d111d497b829ff451d5c96158
     return (
       <div className="myDiv">
         Hello Electron!
