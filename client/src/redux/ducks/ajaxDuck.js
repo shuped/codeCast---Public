fbd6a4514593a7495bd0df4562ee86696143d897
Spencer Shupe <shupe@protonmail.com>
2018-10-27 10:14:07 -0700

    replaced broadcaster with client

--- client/src/redux/ducks/ajaxDuck.js
+++ client/src/redux/ducks/ajaxDuck.js
@@ -1,24 +1,5 @@
-import { updateBroadcasterStreams } from './streamsDuck.js';
-import store from '../store/index.js';
+import { updateViewerStreams } from './streamsDuck.js';
 const axios = require('./api');
-
-export const fetchBroadcasterStreams = (userID) => {
-
-  //ajax get request to server url. Get streams based off userID
-  axios({
-    method: 'get',
-    url: `/api/scheduledStreams?user_id=${userID}`
-  }).then((res) => {
-    console.log(res)
-    const scheduledStreams = JSON.parse(res);
-    store.dispatch(updateBroadcasterStreams(scheduledStreams))
-    console.log(res.data);
-  }).catch((err) => {
-    console.error('Error:', err.data);
-    throw err;
-  });
-  // return {type:null, payload:null}
-}
 
 export const postDeleteStream = (streamID) => {
 
@@ -31,4 +12,23 @@ export const postDeleteStream = (streamID) => {
     console.error('Error:', err.data);
     throw err;
   });
+}
+
+export const fetchViewerStreams = (userID) => {
+  return function (dispatch) {
+    axios({
+      method: 'get',
+      url: `/api/scheduledStreams?user_id=${userID}`
+    }).then((res) => {
+      console.log(res);
+      const scheduledStreams = Object.entries(JSON.parse(res))
+        .map(([streamID, stream]) => [streamID, ...stream]);
+      
+      dispatch(updateViewerStreams(scheduledStreams))
+      return true
+    }).catch((err) => {
+      console.error('Error:', err.data);
+      return false
+    });
+  };
 }
