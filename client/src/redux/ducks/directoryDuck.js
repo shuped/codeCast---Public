b9a64a22cf8dddbef6fd741783d4221c51b084b7
Spencer Shupe <shupe@protonmail.com>
2018-10-27 12:31:43 -0700

    white space refactor

--- client/src/redux/ducks/directoryDuck.js
+++ client/src/redux/ducks/directoryDuck.js
@@ -7,12 +7,7 @@ const FILE_REQUEST = 'server/file_change';
 export const updateFile = (fileID) => ({ type: FILE_REQUEST, payload: { fileID } });
 
 // DIRECTORY REDUCER
-<<<<<<< HEAD
 const directoryState = { directoryStructure: { 'Next Great Thing': null } };
-=======
-const directoryState = {directoryStructure: { 'Next Great Thing': null }};
-
->>>>>>> feature/electronReact
 export const directoryReducer = (state = directoryState, action) => {
   switch (action.type) {
     case 'DIRECTORY_UPDATE':
