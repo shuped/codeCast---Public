e62b483fc4740c09a5e233ff1b5866acc4e5aa0f
Spencer Shupe <shupe@protonmail.com>
2018-10-27 08:24:05 -0700

    create wrapped App in router and redux

--- electronapp/src/index.js
+++ electronapp/src/index.js
@@ -1,12 +1,16 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from './redux/store/index.js';
+import Root from './Root.jsx';
+import * as serviceWorker from './serviceWorker.js';
+import './styles/css/index.css';
 
-var Entry = function(){
-    return (
-      <div className="myDiv">
-        Hello Electron!
-      </div>
-    )
-  }
+ReactDOM.render(
+  <Root store={ store } />, 
+  document.getElementById('root')
+);
 
-ReactDOM.render(<Entry />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
