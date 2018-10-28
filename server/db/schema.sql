4afe15d309be3748b36ef00e2a47f4f58322d6c9
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-27 17:54:57 -0700

    update db schema and implement UUIDv1 PKs

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,15 +1,21 @@
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA pg_catalog;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS streams CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS data CASCADE;
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY default uuid_generate_v1(),
   username text NOT NULL,
   email text NOT NULL UNIQUE,
   password text NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS streams (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  id UUID PRIMARY KEY default uuid_generate_v1(),
+  user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
   status text NOT NULL DEFAULT 'upcoming',
   title text NOT NULL,
   description text NOT NULL,
@@ -20,16 +26,16 @@ CREATE TABLE IF NOT EXISTS streams (
 );
 
 CREATE TABLE IF NOT EXISTS messages (
-  user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  stream_id INTEGER NOT NULL REFERENCES streams (id) ON DELETE CASCADE,
+  user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  stream_id UUID NOT NULL REFERENCES streams (id) ON DELETE CASCADE,
   content text NOT NULL,
   created_at timestamptz,
   PRIMARY KEY (user_id, stream_id)
 );
 
 CREATE TABLE IF NOT EXISTS data (
-  id SERIAL PRIMARY KEY,
-  stream_id INTEGER NOT NULL REFERENCES streams (id) ON DELETE CASCADE,
+  id UUID PRIMARY KEY default uuid_generate_v1(),
+  stream_id UUID NOT NULL REFERENCES streams (id) ON DELETE CASCADE,
   directory JSONB NOT NULL,
   content JSONB NOT NULL
 );
