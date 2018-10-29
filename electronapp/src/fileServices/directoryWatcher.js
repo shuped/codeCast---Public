5ac6ae7074661c714906e6926d51e7a8521ff87e
Spencer Shupe <shupe@protonmail.com>
2018-10-29 00:53:06 -0700

    revert directoryWatcher

--- electronapp/src/fileServices/directoryWatcher.js
+++ electronapp/src/fileServices/directoryWatcher.js
@@ -1,33 +1,42 @@
-chokidar.watch('.', {
-  ignored: /node_modules|\.git/,
-  persistent: true,
-  // followSymlinks: false,
-  // useFsEvents: false,
-  // usePolling: false
-}).on('all', async function(event, path) {
- const eventMethods = {
-   'add': (filePath) => {
-    
-   },
-   'addDir': (filePath) => {
+const chokidar = require('chokidar');
+ const fs = require('fs');
+ const https = require('https');
+ const fsMapper = require('./fs-mapper.js');
+ const path = require('path');
+
+ module.exports = () => chokidar.watch('.', {
+   ignored: /node_modules|\.git/,
+   persistent: true,
+   // followSymlinks: false,
+   // useFsEvents: false,
+   // usePolling: false
+ }).on('all', function(event, path) {
+   const eventMethods = {
+     'add': (fsEvent, filePath) => {
+       console.log('add', filePath)
+      
+ 	  },
+     'addDir': (fsEvent, filePath) => {
+       console.log('addDir',filePath)
 
 
-   },
-   'change': (filePath) => {
-     console.log('change', filePath)
-     readDir(__dirname, done(__dirname))
-   },
-   'unlink': (filePath) => {
-     console.log('unlink', filePath)
+     },
+     'change': (fsEvent, filePath) => {
+       console.log('change', filePath)
 
-   }
- }
- console.log('event, path:', event, path )
- // event specific behavior
- eventMethods[event] ? eventMethods[event](path) : console.log('Event missed:', event);
+     },
+     'unlink': (fsEvent, filePath) => {
+       console.log('unlink', filePath)
 
- // map directory entirely
-})
- .on('ready', function() {
-  console.log('Ready');
-})
+     }
+   };
+   console.log('event, path:', event, path)
+   // event specific behavior
+    eventMethods[event] ? eventMethods[event](path) : console.log('Event missed:', event);
+
+    // map directory entirely
+
+ })
+  .on('ready', function() {
+   console.log('Ready');
+ })
