0d578cad093855002adbca9b35ce78bd47aac4b0
Spencer Shupe <shupe@protonmail.com>
2018-10-29 00:41:29 -0700

    chokidar script to onchange call readdir. needs to send changes only

--- electronapp/src/fileServices/directoryWatcher.js
+++ electronapp/src/fileServices/directoryWatcher.js
@@ -1,57 +1,34 @@
-module.exports = function watch(dirName) {
+chokidar.watch('.', {
+  ignored: /node_modules|\.git/,
+  persistent: true,
+  // followSymlinks: false,
+  // useFsEvents: false,
+  // usePolling: false
+}).on('all', async function(event, path) {
+ const eventMethods = {
+   'add': (filePath) => {
+    
+   },
+   'addDir': (filePath) => {
 
-  const chokidar = require('chokidar');
-  const watcher = chokidar.watch('file, dir', {
-    ignored: /(^|[\/\\])\../ + '**/node_modules/, **package-lock.json',
-    persistent: true,
-    ignoreInitial: false,
-    followSymlinks: false,
-    cwd: dirName,
-    disableGlobbing: false,
-    usePolling: true,
-    interval: 100,
-    binaryInterval: 300,
-    alwaysStat: false,
-    depth: 99,
-    awaitWriteFinish: {
-      stabilityThreshold: 2000,
-      pollInterval: 100
-    },
-    ignorePermissionErrors: true
-  });
 
-  const log = console.log.bind(console);
+   },
+   'change': (filePath) => {
+     console.log('change', filePath)
+     readDir(__dirname, done(__dirname))
+   },
+   'unlink': (filePath) => {
+     console.log('unlink', filePath)
 
-  function watcherReady() {
-    log('Watcher is lurking');
-  }
-  //bind console.log
-  // Add event listeners.  
-  // 'add', 'addDir' and 'change' events also receive stat() results as second
-  // argument when available: http://nodejs.org/api/fs.html#fs_class_fs_stats
-  watcher.on('add', (path, stats) => {
-    log(`File ${path} was added;\nStats: ${stats}`);
+   }
+ }
+ console.log('event, path:', event, path )
+ // event specific behavior
+ eventMethods[event] ? eventMethods[event](path) : console.log('Event missed:', event);
 
-  }).on('addDir', (path, stats) => {
-    log(`Directory ${path} added;\nStats: ${stats}`);
-
-  }).on('change', (path, stats) => {
-    log(`File ${path} changed;\nStats: ${stats}`);
-
-  }).on('unlink', (path) => {
-    log(`File ${path} removed;`);
-
-  }).on('unlinkDir', (path) => {
-    log(`Directory ${path} removed;`);
-
-  }).on('error', (err) => {
-    log(`Chokidar error: ${err}`);
-
-  }).on('ready', watcherReady)
-  .on('raw', (event, path, details) => {
-    // all events register as raw; use for testing purposes 
-    log('Raw event data:', event, path, details);
-  });
-
-  return watcher;
-};
+ // map directory entirely
+})
+ .on('ready', function() {
+  console.log('Ready');
+})
+});
