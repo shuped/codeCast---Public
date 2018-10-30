6107d02b90c8f5b9f47a456c7451ff469d690738
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-29 18:28:22 -0700

    modify app

--- electronapp/src/App.jsx
+++ electronapp/src/App.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 //components
 import LiveStreamNow from './ElectronDashboard/LiveStreamNow.jsx';
 import ScheduleNewStream from './ElectronDashboard/ScheduleNewStream.jsx';
+import StartScheduled from './ElectronDashboard/StartScheduled.jsx';
+import ElectronDashboard from './ElectronDashboard/ElectronDashboard.jsx';
+
 // import logo from './assets/logo.svg';
 
 class App extends Component {
