ad87b42d241fa8bf3011113f6fd0bdec7b37dc3a
Spencer Shupe <shupe@protonmail.com>
2018-10-30 04:32:56 -0700

    capitalization errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 //components
-import IndexViews from './ViewComponents/IndexViews/IndexViews.jsx';
-import Header from './ViewComponents/Header.jsx'
-
+// import IndexViews from './ViewComponents/IndexViews/IndexViews.jsx';
+// import Header from './ViewComponents/Header.jsx'
+import StreamIndex from './StreamComponents/StreamIndex.jsx'
 
 // import logo from './assets/logo.svg';
 
@@ -13,14 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <Header />
-        </div>
-        <div>
-          <IndexViews />
-        </div>
-
-
+        <StreamIndex />
       </div>
     );
   }
