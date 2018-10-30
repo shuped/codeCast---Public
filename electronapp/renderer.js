c0f9c676cbba181b2908b8084ad1e9fabedea487
Spencer Shupe <shupe@protonmail.com>
2018-10-30 12:22:12 -0700

    move chokidar directory reader to module, require in renderer.js

--- electronapp/renderer.js
+++ electronapp/renderer.js
@@ -2,15 +2,15 @@ const os = require('os');
 const pty = require('node-pty');
 const Terminal = require('xterm').Terminal;
 const socket = require('socket.io-client');
-
+const directoryWatcher = require('./src/fileServices/directoryWatcher.js')
+const path = require('path')
 const io = socket.connect('http://localhost:8080/terminal');
 
 // Initialize node-pty with an appropriate shell
 const shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
 const ptyProcess = pty.spawn(shell, [], {
   name: 'xterm-color',
-  cols: 80,
-  rows: 30,
+
   cwd: process.cwd(),
   env: process.env
 });
@@ -27,41 +27,7 @@ xterm1.on('data', (data) => {
 
 ptyProcess.on('data', function (data) {
   xterm1.write(data);
-  terminalIO.emit('data', data)
+  io.emit('data', data)
 });
 
-chokidar.watch('.', {
-  ignored: /node_modules|\.git/,
-  persistent: true,
-  // followSymlinks: false,
-  // useFsEvents: false,
-  // usePolling: false
-}).on('all', function(event, path) {
-  const eventMethods = {
-    'add': (fsEvent, filePath) => {
-      console.log('add')
-      
-	  },
-    'addDir': (fsEvent, filePath) => {
-      console.log('addDir')
-
-
-    },
-    'change': (fsEvent, filePath) => {
-      console.log('add3')
-
-    },
-    'unlink': (fsEvent, filePath) => {
-      console.log('add4')
-
-    }
-  };
-  console.log('event, path:', event, path)
-  if (fs.existsSync('./directory.json') && fs.existsSync('./content.json')) {
-    eventMethods[event] ? eventMethods[event](path) : console.log('Event missed:', event);
-  } else {
-    // map directory
-  }
-}).on('ready', function() {
-  console.log('Ready');
-})
+directoryWatcher(path.join(__dirname, '..'));
--- electronapp/src/fileServices/directoryWatcher.js
+++ electronapp/src/fileServices/directoryWatcher.js
@@ -1,42 +1,44 @@
 const chokidar = require('chokidar');
  const fs = require('fs');
  const https = require('https');
- const fsMapper = require('./fs-mapper.js');
+ const { readDir, done } = require('./fs-mapper.js');
  const path = require('path');
 
- module.exports = () => chokidar.watch('.', {
-   ignored: /node_modules|\.git/,
-   persistent: true,
-   // followSymlinks: false,
-   // useFsEvents: false,
-   // usePolling: false
- }).on('all', function(event, path) {
-   const eventMethods = {
-     'add': (fsEvent, filePath) => {
-       console.log('add', filePath)
-      
- 	  },
-     'addDir': (fsEvent, filePath) => {
-       console.log('addDir',filePath)
+ module.exports = (dirToWatch) => 	chokidar.watch(dirToWatch, {
+  ignored: /node_modules|\.git/,
+  persistent: true,
+  ignoreInitial: true
+  // followSymlinks: false,
+  // useFsEvents: false,
+  // usePolling: false
+}).on('all', function(event, pathArg) {
+  const eventMethods = {
+  // TODO: SEND ONLY CHANGES IN FUTURE
+  'add': (filePath) => {
+    console.log('add', filePath);
+    readDir(dirToWatch, done(dirToWatch));
+    
+  },
+  'addDir': (filePath) => {
+    console.log('addDir', filePath);
+    readDir(dirToWatch, done(dirToWatch));
 
+  },
+  'change': (filePath) => {
+    console.log('change', filePath);
+    readDir(dirToWatch, done(dirToWatch));
 
-     },
-     'change': (fsEvent, filePath) => {
-       console.log('change', filePath)
-
-     },
-     'unlink': (fsEvent, filePath) => {
-       console.log('unlink', filePath)
-
-     }
-   };
-   console.log('event, path:', event, path)
-   // event specific behavior
-    eventMethods[event] ? eventMethods[event](path) : console.log('Event missed:', event);
-
-    // map directory entirely
-
- })
-  .on('ready', function() {
-   console.log('Ready');
- })
+  },
+  'unlink': (filePath) => {
+    console.log('unlink', filePath);
+    readDir(dirToWatch, done(dirToWatch));
+  }
+};
+  console.log('event, path:', event, pathArg);  
+  // event specific behavior;s
+  eventMethods[event] ? eventMethods[event](pathArg) : console.log('Event missed:', event);
+}).on('ready', async function() {
+  console.log('Ready');
+  readDir(dirToWatch, done(dirToWatch));
+//  TODO: Move the axios to here instead of fs-mapper
+});
