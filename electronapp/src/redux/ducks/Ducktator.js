6a7ea9f2b66e3c229b0939569793d9ab56cea6bc
erik-mackie <erik.mackie@gmail.com>
2018-10-30 09:24:33 +0000

    merge conflicts with spencer5

--- electronapp/src/redux/ducks/Ducktator.js
+++ electronapp/src/redux/ducks/Ducktator.js
@@ -4,7 +4,3 @@ import { combineReducers } from 'redux'
 export const rootReducer = combineReducers({
   streams: streamsReducer
 })
-<<<<<<< HEAD
-
-=======
->>>>>>> c2f3d8eff3ab858cbae8fa9d748e1c46b9894095
