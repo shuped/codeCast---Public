9559454b798e2563c03cad00a7ee5f752d2f1781
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-31 02:23:37 -0700

    minor reformatting

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { postgraphile } = require('postgraphile');
 const path             = require('path');
 const morgan           = require('morgan');
 const bodyParser       = require('body-parser');
-const uuid             = require('uuid/v1')
+const uuid             = require('uuid/v1');
 const PORT             = process.env.PORT;
 
 const activeData       = require('./testData/activeData.json');
@@ -32,20 +32,8 @@ let pathCache          = null;
 //   'bodySizeLimit': '50mb'
 // }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
