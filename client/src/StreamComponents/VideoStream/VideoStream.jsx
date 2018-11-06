a181327768d4c52b8e5f9c479cf65019cd0c6694
Spencer Shupe <shupe@protonmail.com>
2018-11-05 17:30:50 -0800

    api call for youtubeURL of stream

--- client/src/StreamComponents/VideoStream/VideoStream.jsx
+++ client/src/StreamComponents/VideoStream/VideoStream.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import axios from '../../redux/ducks/api.js';
 
 class Video extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {url: 'https://www.youtube.com/embed/'}
+  };
+
+  componentDidMount() {
+    const { streamID } = this.props;
+    axios({
+      method: 'get',
+      url: `/api/query?id=${streamID}&field=youtubeURL`
+    }).then((res) => {
+      this.setState({url: res.data})
+    });
+  };
+
   render() {
+    console.log(this.state)
     return (
       <div className='video-main'>
-        <iframe className='iframe' src="https://www.youtube.com/embed/KY2kM3Tzg-w" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        <iframe className='iframe' src={this.state.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
       </div>
     );
   };
-}
+};
 
 export default Video;
