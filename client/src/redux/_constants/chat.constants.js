bee8e90924bc3e472af27607851d63a3eea17ff8
Erik Mackie <erik@erik.workStation>
2018-11-14 15:55:21 -0800

    redux folders added, exported chat constants

---
+++ client/src/redux/_constants/chat.constants.js
@@ -0,0 +1,8 @@
+export const chatConstants = {
+  INCOMING_MESSAGE:      'NEW_MESSAGE',
+  INCOMING_NOTIFICATION: 'NEW_NOTIFICATION',
+  USERNAME_CHANGE:       'USERNAME_CHANGE',
+
+  OUTGOING_MESSAGE:      'server/message',
+  NEW_CONNECTION :       'server/new_connection'
+}
