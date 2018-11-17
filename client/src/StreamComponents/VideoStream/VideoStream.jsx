d3bfb906780455d97f7c9681d72d2f56ee49bf33
Spencer Shupe <shupe@protonmail.com>
2018-11-16 19:56:22 -0800

    remove axios call for URL in videoStream

--- client/src/StreamComponents/VideoStream/VideoStream.jsx
+++ client/src/StreamComponents/VideoStream/VideoStream.jsx
@@ -4,24 +4,12 @@ import axios from '../../redux/api.js';
 class Video extends Component {
   constructor(props) {
     super(props);
-    this.state = {url: 'https://www.youtube.com/embed/'}
-  };
-
-  componentDidMount() {
-    const { streamID } = this.props;
-    axios({
-      method: 'get',
-      url: `/api/query?id=${streamID}&field=youtubeURL`
-    }).then((res) => {
-      this.setState({url: res.data})
-    });
   };
 
   render() {
-    console.log(this.state)
     return (
       <div className='video-main'>
-        <iframe className='iframe' src={this.state.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        <iframe className='iframe' src={this.props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
       </div>
     );
   };
