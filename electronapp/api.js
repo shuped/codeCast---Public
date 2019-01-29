ba4243763dba56e29359d65b3c410e31a2f305e7
Spencer Shupe <shupe@protonmail.com>
2019-01-28 20:12:33 -0800

    testing server CORS, quick switch from localhost

--- electronapp/api.js
+++ electronapp/api.js
@@ -2,6 +2,6 @@ const axios = require('axios');
 
 module.exports = axios.create({
   timeout: 60000,
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'http://codecastserver-prod.us-west-2.elasticbeanstalk.com/:8080/',
   maxContentLength: ((5000 * 1000) * 1000)
 });
