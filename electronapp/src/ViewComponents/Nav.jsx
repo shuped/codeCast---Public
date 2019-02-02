8d8b92fc1af47ed8b5995d24fcbe62270fd678b4
Spencer Shupe <shupe@protonmail.com>
2019-02-02 14:08:50 -0800

    moves stream controls to nav, in lieu of logo

--- electronapp/src/ViewComponents/Nav.jsx
+++ electronapp/src/ViewComponents/Nav.jsx
@@ -5,14 +5,15 @@ import CodecastLogo from '../assets/CODECAST_LOGO.png';
 function Nav() {
   return(
     <header className="nav-header">
-      <NavLink to="/"><img className="nav-logo" src={ CodecastLogo }></img></NavLink>
-      <div className="nav-links">
+      {/* <NavLink to="/"><img className="nav-logo" src={ CodecastLogo }></img></NavLink> */}
+
+      {/*nav-links and nav-controls*/}
         <NavLink className="btn home nav-link" to="/">Dashboard</NavLink>
 
-        <NavLink className="btn start-now nav-link" to="/LiveStreamNow">Stream Now</NavLink>
+        <NavLink className="btn start-now nav-link" to="/LiveStreamNow">Stream Now!</NavLink>
+        
+        <NavLink className="btn start-scheduled nav-link" to='/ScheduleNewStream' id="scheduleNew" >Schedule a Stream</NavLink>
 
-        {/* <NavLink className="btn schedule-new nav-link" to="/ScheduleNewStream">Schedule New</NavLink> */}
-      </div>
     </header>
   );
 }
