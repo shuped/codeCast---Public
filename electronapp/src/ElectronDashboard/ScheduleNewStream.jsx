a063fce2203bf7e472384207397c70cbb8ba35cc
Spencer Shupe <shupe@protonmail.com>
2019-02-02 20:40:29 -0800

    add fields for scheduled stream

--- electronapp/src/ElectronDashboard/ScheduleNewStream.jsx
+++ electronapp/src/ElectronDashboard/ScheduleNewStream.jsx
@@ -1,91 +1,110 @@
-import React from 'react';
-import { Button } from 'antd';
-import { Input, Select, DatePicker } from 'antd';
-import { connect } from 'react-redux'; 
+import React from "react";
+import { Button } from "antd";
+import { Input, Select, DatePicker } from "antd";
+import { connect } from "react-redux";
 
-import { postScheduledStream } from '../redux/ducks/streamsDuck.js'
-import { withRouter, Link, Redirect } from 'react-router-dom';
+import { postScheduledStream } from "../redux/ducks/streamsDuck.js";
+import { withRouter, Link, Redirect } from "react-router-dom";
 
 const InputGroup = Input.Group;
 const Option = Select.Option;
 
 class ScheduleNewStream extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      title: 'Title your stream...',
-      userID: 'userID',
-      user: 'userName',
-      description: 'What are you coding today?',
-      scheduledDate: 'NOW!',
-      languageImage: 'javascript'
+      title: "Untitled",
+      userID: "Anon",
+      user: "anon",
+      description: "No description provided. Could be interesting!",
+      scheduledDate: "",
+      languageImage: "javascript"
     };
   }
 
   //handlers
-  TitleOnChange = (event) => {
-    this.setState({title: event.target.value});
-  }
-  
-  DescriptionOnChange = (event) => {
-    this.setState({description: event.target.value})
-  }
+  HandleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-  onChange = (value) => {
-    this.setState({scheduledDate: value})
-  }
-  
-  onOk = (value) => {
-    this.setState({scheduledDate: value['_d']})
+  onOk = value => {
+    this.setState({ scheduledDate: value["_d"] });
     // console.log('onOk: ', value);
-  }
+  };
 
-  HandleImageSelection = (value) => {
-    this.setState({languageImage: value});
-    console.log(value)
-  }
+  HandleImageSelection = value => {
+    this.setState({ languageImage: value });
+    console.log(value);
+  };
 
-  HandleSubmit = (event) => {
+  HandleSubmit = event => {
     event.preventDefault();
     //redirect to broadcast page
     this.props.postScheduledStream(this.state);
-    console.log(this.state)
-    this.props.history.push('/');
-  }
+    console.log(this.state);
+    this.props.history.push("/");
+  };
 
-  render() {  
-    
+  render() {
     return (
+      <main className="new-stream">
+        <div className="inputs-container">
+          <h1 className="container-header">Schedule A Stream</h1>
 
-        <main className="new-stream">          
-          <div className="inputs-container">
-            <h1 className="container-header">Schedule A Stream</h1>
-
-            <div className="forms-container">
-              <form id="form" onSubmit={this.HandleSubmit}>
-                <div className="title-input">
+          <div className="forms-container">
+            <form onSubmit={this.HandleSubmit}>
+              <div className="title-input">
+                <div className="">
                   <h3>Title:</h3>
-                  <input type="text" title={this.state.title} onChange={this.TitleOnChange} />
+                  <input
+                    type="text"
+                    placeholder="Title..."
+                    name="title"
+                    onChange={this.HandleInputChange}
+                  />
                 </div>
-                <div className="description-input">
-                  <h3>Description:</h3>
-                  <textarea type="text" rows="4" cols="90" description={this.state.description} onChange={this.DescriptionOnChange} />
+                <div className="">
+                  <h3>Username:</h3>
+                  <input
+                    type="text"
+                    placeholder="John Carmack..."
+                    name="user"
+                    onChange={this.HandleInputChange}
+                  />
                 </div>
-                <div className=" bottom-container">
-                  
-                  <input type="submit" value="Submit" />
-                </div>
-              </form>
-            </div>
+              </div>
+
+              <div className="description-input">
+                <h3>Description</h3>
+                <textarea
+                  type="text"
+                  placeholder="What project are you sharing today?"
+                  rows="4"
+                  cols="90"
+                  name="description"
+                  onChange={this.HandleInputChange}
+                />
+              </div>
+
+
+              <div className=" bottom-container">
+                <h3>Date:</h3>
+                <input type="text" placeholder="Type your date..." name='scheduledDate' onChange={this.HandleInputChange} />
+                
+              </div>
+
+              <div className="b-bottom-container">
+                <input className="launch-btn schedule-btn" type="submit" value="Schedule!" />
+              </div>
+            </form>
           </div>
-        </main>
-      
+        </div>
+      </main>
     );
   }
 }
 
-// leave in 
+// leave in
 
 // const mapStateToProps = (state) => {
 //   return {
@@ -93,12 +112,15 @@ class ScheduleNewStream extends React.Component {
 //   }
 // }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    postScheduledStream: (streamData) => dispatch(postScheduledStream(streamData))
-  }
-}
+    postScheduledStream: streamData => dispatch(postScheduledStream(streamData))
+  };
+};
 
-
-export default withRouter(connect(null, mapDispatchToProps)(ScheduleNewStream));
-
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(ScheduleNewStream)
+);
