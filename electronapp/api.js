88d4b75ef4fd15e52f67b3ad784d474d36621734
Spencer Shupe <shupe@protonmail.com>
2019-02-04 04:55:07 -0800

    .env variable use case

--- electronapp/api.js
+++ electronapp/api.js
@@ -2,6 +2,6 @@ const axios = require('axios');
 
 module.exports = axios.create({
   timeout: 60000,
-  baseURL: 'http://codeCastServer-prod.us-west-2.elasticbeanstalk.com/',
+  baseURL: 'http://localhost:8080/',
   maxContentLength: ((5000 * 1000) * 1000)
 });
