f7ff01f971c7ce3c9a547a1d23082546ce00fde3
Spencer Shupe <shupe@protonmail.com>
2019-02-04 06:26:46 -0800

    new Date in initial messages was messing with sorting

--- client/src/StreamComponents/Chat/messages/initialMessageUtil.js
+++ client/src/StreamComponents/Chat/messages/initialMessageUtil.js
@@ -5,28 +5,28 @@ export default [
   {
     type: 'messages',
     id: uuid(),
-    timestamp: new Date(),
+    timestamp: new Date(0),
     user: { username: 'System', userColor: '#FF00FF' },
     content: '/bold(Welcome!) This is the chat application to talk to connected viewers and the broadcaster.'
   },
   {
     type: 'messages',
     id: uuid(),
-    timestamp: new Date(),
+    timestamp: new Date(0),
     user: { username: 'System', userColor: '#FF00FF' },
     content: 'It supports /bold(commands preceded by a forward slash) like ital() and bold() to decorate text between the parentheses.'
   },
   {
     type: 'messages',
     id: uuid(),
-    timestamp: new Date(),
+    timestamp: new Date(0),
     user: { username: 'System', userColor: '#FF00FF' },
     content: 'To send a /bold(link or image), use img(your_link) or link(your_link) commands, again preceded by a "/".'
   },
   {
     type: 'messages',
     id: uuid(),
-    timestamp: new Date(),
+    timestamp: new Date(0),
     user: { username: 'System', userColor: '#FF00FF' },
     content: 'To /bold(notify the broadcaster) about a message, use the @broadcaster command.'
   }
